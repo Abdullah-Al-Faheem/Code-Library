33fb4b234b7cc732cc65197923474dcc1b7bc4ab
Abdullah Al Fahim <138691546+Abdullah-Al-Faheem@users.noreply.github.com>
2026-02-18 15:46:35 +0600

    Refactor code logic

--- stress_tester/code.cpp
+++ stress_tester/code.cpp
@@ -8,40 +8,26 @@ int32_t main()
 {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
-    int n; cin >> n;
-    vector<int> arr(n+3, 0); 
-    for (int i = 1; i <= n; i++) cin>> arr[i];
+    int n,a,c1=0,c0=0,mx=-1;
 
-    int curr, mx;
-    curr = mx = (arr[1]?-1:1);
+    cin>>n;
 
-    int l, r, lft, rgt; 
-    l = r = lft = rgt = 1;
-
-    for (int i = 2; i <= n; i++)
+    while(n--)
     {
-        int val = (arr[i]?-1:1);
+        cin>>a;
 
-        if( val > (val+curr) ) 
+        if(a)
         {
-            curr = val;
-            l = r = i;
-        }
-        else 
-        {
-            curr+= val;
-            r=i;
-        }
+            c1++;
 
-        if(mx<curr)
+            if(c0)c0--;
+        }
+        else
         {
-            mx = curr;
-            lft = l, rgt =r;
+            c0++;
+            mx = max(c0, mx);
         }
     }
 
-    for (int i = lft; i <= rgt; i++) arr[i]^=1;
-
-    int sum =accumulate(all(arr), 0); 
-    cout<<sum<<nl;
+    cout<<c1+mx<<endl;
 }
